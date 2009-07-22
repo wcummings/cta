3987557bd97a04fc4f3000d8bc52133acf5bf7b9
William Cummings <wcummings22@gmail.com>
2009-07-22 16:56:16 -0400

    Added view param.
    view=id displays the quote with the appropriate id.

--- cta.pl
+++ cta.pl
@@ -11,9 +11,10 @@ my $dbfile = 'cta.sqlite';
 my $name = param('name');
 my $text = param('text');
 
-my $id   = url_param('id');
-my $op   = url_param('op');
+my $id    = url_param('id');
+my $op    = url_param('op');
 my $order = url_param('order');
+my $view  = url_param('view');
 
 my $db = DBI->connect("dbi:SQLite:dbname=$dbfile", "", "") 
     or die "Couldn't connect to db: $DBI::errstr";
@@ -39,20 +40,28 @@ if($id && $op) {
 print "<html>\n";
 print "<head><title>Chicago Transit Authority</title></head>\n";
 print "<body>\n";
-print "<h2>CTA - Insert Quotes Here</h2>\n";
-print "<form method='post' action='cta.cgi' name='cta_input'>\n";
-print "<label for='name'>Username (HONOR SYSTEM FTW!)</label>\n";
-print "<input type='text' name='name' value='$name' size='20'/><br/>\n";
-print "<textarea rows='10' cols='80' name='text'></textarea><br/>\n";
-print "<input type='submit'/>\n";
-print "</form>\n";
+if(not $view) {
+    print "<h2>CTA - Insert Quotes Here</h2>\n";
+    print "<form method='post' action='cta.cgi' name='cta_input'>\n";
+    print "<label for='name'>Username (HONOR SYSTEM FTW!)</label>\n";
+    print "<input type='text' name='name' value='$name' size='20'/><br/>\n";
+    print "<textarea rows='10' cols='80' name='text'></textarea><br/>\n";
+    print "<input type='submit'/>\n";
+    print "</form>\n";
+}
 
-my $order_by = ($order eq "rating") ? "rating" : "date";
-my $query = $db->prepare("SELECT id, name, text, date, rating FROM cta ORDER BY $order_by DESC");
-$query->execute();
+my $query;
+if($view) {
+    $query = $db->prepare("SELECT id, name, text, date, rating FROM cta WHERE id = ?");
+    $query->execute($view);
+} else {
+    my $order_by = ($order eq "rating") ? "rating" : "date";
+    $query = $db->prepare("SELECT id, name, text, date, rating FROM cta ORDER BY $order_by DESC");
+    $query->execute();
+}
 
 my $url = url(-full => 1);
-print "Order by <a href='$url'>date</a> / <a href='$url?order=rating'>rating</a><br/>";
+print "Order by <a href='$url'>date</a> / <a href='$url?order=rating'>rating</a><br/>" if not $view;
 
 while(my @row = $query->fetchrow_array) {
     my $o = "";
@@ -61,9 +70,10 @@ while(my @row = $query->fetchrow_array) {
 
     my $up_url = $url . "?id=$row[0]&op=up$o";
     my $down_url = $url . "?id=$row[0]&op=down$o";
+    my $view_url = $url . "?view=$row[0]";
 
     print "<p>\n";
-    print "<u>\#$row[0]</u> by $row[1] at $row[3]<br/>\n";
+    print "<a href='$view_url'>\#$row[0]</a> by $row[1] at $row[3]<br/>\n";
     print "<pre>$row[2]</pre>\n";
     print "<small><i>Rated $row[4]</i> (<a href='$up_url'>up</a>/<a href='$down_url'>down</a>)</small>";
     print "</p>\n";
